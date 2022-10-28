2e5a3cf9d2f8a0329b1780e87b69b88ef2060a58
Maggie Lively <maggielively@Maggie-Livelys-Pro.local>
2022-10-28 17:08:44 -0400

    additions to readMe but still need instructional video, css updates

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Team_Profile_Generator
+
+## Table of Contents
+- [Project description](#Description)
+- [Usage](#Usage)
+- [Installation](#Installation)
+- [Contributions](#Contributions)
+- [Questions](#Questions)
+
+## Description
 This homework project requires building a Node.js command-line application that takes in information about employees team and generates results on an HTML page.
+
+## Usage
+Use the command line to generate a team profile list with their details.
+https://mmllively.github.io/Team_Profile_Generator/
+![alt text](./Screen%20Shot%202022-10-28%20at%205.07.03%20PM.png)
+
+
+## Installation
+Node.js & Inquirer 8.2.4
+
+## Contributions
+UCF TA's and fellow classmates.
+
+## Questions - Link to Instructional Video
+
+
+
+
--- src/page-template.js
+++ src/page-template.js
@@ -16,8 +16,8 @@ const html = [];
        </div>
      </div>
   </div>
-  </div>
-     `;
+  </div>`
+     ;
      html.push(managerHtml);
     };
 
@@ -37,8 +37,8 @@ const html = [];
         </div>
       </div>
       </div>
-  </div>
-      `;
+  </div>`
+      ;
       html.push(engineerHtml);
     };
 
@@ -58,9 +58,8 @@ const html = [];
         </div>
       </div>
       </div>
-  </div>
- 
-      `;
+  </div>`
+      ;
       html.push(internHtml);
     };
 
@@ -91,6 +90,7 @@ module.exports = team => {
        <meta http-equiv="X-UA-Compatible" content="IE=edge">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+       <link rel = "stylesheet" href = "style.css">
        <title>Document</title>
    </head>
    <div class="jumbotron jumbotron-fluid bg-success">
@@ -100,6 +100,7 @@ module.exports = team => {
  </div>
    <body>
    <div class= "container">
+   <div class="row row-cols-3">
    ${generateTeam(team)}
    </div>
    </body>
